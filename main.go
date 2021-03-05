6a608d1a6d7dcb9cd30a8f5689dcc31e5d0bf5ce
Jeisson P <jeisson005@hotmail.com>
2021-03-05 00:07:20 -0500

    feat: Refactor #1

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	_ "github.com/udistrital/tesoreria_mid/routers"
-
 	"github.com/astaxie/beego"
+	_ "github.com/udistrital/tesoreria_mid/routers"
+	"github.com/udistrital/utils_oas/customerrorv2"
 )
 
 func main() {
@@ -11,5 +11,6 @@ func main() {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+	beego.ErrorController(&customerrorv2.CustomErrorController{})
 	beego.Run()
 }
