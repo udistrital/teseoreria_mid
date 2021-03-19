031cafbf29e49f66d7150f7769fa2658431cf65c
Jeisson P <jeisson005@hotmail.com>
2021-03-19 11:25:04 -0500

    feat: Se habilitan peticiones para evitar bloqueo por cors #2

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/plugins/cors"
 	_ "github.com/udistrital/tesoreria_mid/routers"
 	"github.com/udistrital/utils_oas/customerrorv2"
 )
@@ -11,6 +12,18 @@ func main() {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders: []string{"Origin", "x-requested-with",
+			"content-type",
+			"accept",
+			"origin",
+			"authorization",
+			"x-csrftoken"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 	beego.ErrorController(&customerrorv2.CustomErrorController{})
 	beego.Run()
 }
