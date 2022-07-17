e87d3ad23639ba36e332bf3d023a1f6547c9be93
Alex F. Bustos P <alexfbp@gmail.com>
2022-07-16 22:10:06 -0500

    feat: auditoria de utils_oas

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/plugins/cors"
+
 	_ "github.com/udistrital/tesoreria_mid/routers"
-	"github.com/udistrital/utils_oas/customerror"
 	apistatus "github.com/udistrital/utils_oas/apiStatusLib"
+	"github.com/udistrital/utils_oas/auditoria"
+	"github.com/udistrital/utils_oas/customerror"
 )
 
 func main() {
@@ -14,18 +16,18 @@ func main() {
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
 	}
 	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
-        AllowOrigins: []string{"*"},
-        AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
-        AllowHeaders: []string{"Origin", "x-requested-with",
-            "content-type",
-            "accept",
-            "origin",
-            "authorization",
-            "x-csrftoken"},
-        ExposeHeaders:    []string{"Content-Length"},
-        AllowCredentials: true,
-    }))
-
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders: []string{"Origin", "x-requested-with",
+			"content-type",
+			"accept",
+			"origin",
+			"authorization",
+			"x-csrftoken"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+	auditoria.InitMiddleware()
 	beego.ErrorController(&customerror.CustomErrorController{})
 	apistatus.Init()
 	beego.Run()
